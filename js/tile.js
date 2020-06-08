fcfa1690604ebb880e6af8c4b4b80e8176f8d04a
DiegoReports <diego.reports@gmail.com>
2020-06-08 15:01:25 -0300

    Update-Effects-Document

---
+++ js/tile.js
@@ -0,0 +1,51 @@
+	//====================== Mudança Icones ===========================//
+
+    function Robot() {
+        document.getElementById('robot').src="images/robotOn.png"
+    }
+    
+    function RobotReset() {
+        document.getElementById('robot').src="images/robotOff.png"
+    }
+    
+    function Roboted () {
+        document.getElementById('roboted').src="images/smartOn.png"
+    }
+    function RobotedReset () {
+        document.getElementById('roboted').src="images/smartOff.png"
+    }
+    
+    function Judge() {
+        document.getElementById('judge').src="images/leiOn.png"
+    }
+    function JudgeReset() {
+        document.getElementById('judge').src="images/leiOff.png"
+    }
+    
+    function Banco() {
+        document.getElementById('banco').src="images/bancoOn.png"
+    }
+    function BancoReset() {
+        document.getElementById('banco').src="images/bancoOff.png"
+    }
+    
+    function Doc() {
+        document.getElementById('doc').src="images/docOn.png"
+    }
+    function DocReset() {
+        document.getElementById('doc').src="images/docOff.png"
+    }
+    
+    function Cheque() {
+        document.getElementById('cheque').src="images/chequeOn.png"
+    }
+    function ChequeReset() {
+        document.getElementById('cheque').src="images/chequeOff.png"
+    }
+    
+    function Transf() {
+        document.getElementById('transf').src="images/transfOn.png"
+    }
+    function TransfReset() {
+        document.getElementById('transf').src="images/transfOff.png"
+    }
